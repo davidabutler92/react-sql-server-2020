3dd0c766c93d0897085e2bf14faed81efdced99d
davidabutler92 <davidabutler92@yahoo.com>
2020-11-05 10:53:17 -0800

    add form and submit new snowboard

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import request from 'superagent';
 
 const user = {
-    userId: 1
+    userId: 1,
 }
 
 export default class CreatePage extends Component {
 
     state = {
-        brands: []
+        brands: [],  
+        snowboardName: '',
+        flex: '',
+        isAllMountain: '',
+        brandId: '' 
     }
 
     componentDidMount = async () => {
@@ -19,23 +23,45 @@ export default class CreatePage extends Component {
     handleSubmit = async (e) => {
         e.preventDefault()
 
-        const newSowboard = {
-            snowbaord_name: this.state.snowbaordName,
+        const newSnowboard = {
+            snowboard_name: this.state.snowboardName,
             flex: this.state.flex,
             is_all_mountain: this.state.isAllMountain,
             brand_id: this.state.brandId,
-            owner_id: user
+            owner_id: user.userId
         };
 
         await request
         .post('https://secret-scrubland-39461.herokuapp.com/snowboards')
-        .send(newSowboard);
+        .send(newSnowboard);
+    }
+
+    handleChange = (e) => {
+        this.setState({ brandId: e.target.value })
     }
 
     render() {
         return (
             <div>
-                Create page
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input onChange={e => this.setState({ snowboardName: e.target.value })} placeholder='Snowboard Name'></input>
+                    </label>
+                    <label>
+                        <input type='number' onChange={e => this.setState({ flex: e.target.value })} placeholder='Snowboard Flex'></input>
+                    </label>
+                    <select onChange={e => this.setState({ isAllMountain: e.target.value })}>
+                        <option value='true'>Yes</option>
+                        <option value='false'>No</option>
+                    </select>
+                    <select onChange={e => this.setState({ brandId: e.target.value })}>
+                        {
+                            this.state.brands.map (brand => <option value={ brand.id } key={ brand.id }>{ brand.name }</option>)
+                            
+                        }
+                    </select>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
