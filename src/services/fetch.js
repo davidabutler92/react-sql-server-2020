ad74700306d456dc364d2b9709a2847c37f52aa8
davidabutler92 <davidabutler92@yahoo.com>
2020-11-05 17:11:21 -0800

    add interpolate URL to be used with snowboards

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,10 +1,10 @@
 import request from 'superagent';
 
-const URL = 'https://secret-scrubland-39461.herokuapp.com/snowboards'
+const URL = 'https://secret-scrubland-39461.herokuapp.com/'
 
 export async function getAllSnowboards() {
     try {
-        const response = await request.get(`${URL}`);
+        const response = await request.get(`${URL}snowboards`);
         return response.body;
     } catch(err) {
         throw err;
